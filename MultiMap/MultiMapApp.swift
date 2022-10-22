f6444c9d1757fda5924605b1f5554715dd2ecce5
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-10-22 08:58:01 +0200

    Initial Commit

---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Sebastien REMY on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
